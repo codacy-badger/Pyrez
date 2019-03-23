61798b67cdb181aea25df568b4162eb3fc5f65e1
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-03-23 00:04:38 -0300

    Update http.py

--- pyrez/http.py
+++ pyrez/http.py
@@ -1,18 +1,26 @@
 from sys import version_info as pythonVersion
 
-import aiohttp
+import requests_async as requests
 
 class HttpRequest():
     defaultHeaders = { "User-Agent": "HttpRequestWrapper [Python/{0.major}.{0.minor} aiohttp/{1}]".format(pythonVersion, aiohttp.__version__) }
     timeout = 500
 
-    def __init__(self, loop=None, headers=defaultHeaders):
+    def __init__(self, headers=defaultHeaders):
         self.headers=defaultHeaders if headers is None else headers
 
-    async def get(self, url, params=None, data=None, headers=defaultHeaders, cookies=None, files=None, auth=None, timeout=None, allowRedirects=False, proxies=None, hooks=None, stream=False, verify=None, cert=None):
-        return await self.request('GET', url=url.replace(' ', '%20'), params=params, data=data, headers=headers, cookies=cookies, files=files, auth=auth, timeout=timeout, allowRedirects=allowRedirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert)
-    async def request(self, method, url, params=None, data=None, headers=defaultHeaders, cookies=None, files=None, auth=None, timeout=None, allowRedirects=False, proxies=None, hooks=None, stream=False, verify=None, cert=None):
-        async with aiohttp.ClientSession(cookies=cookies, headers=headers) as session:
-            async with session.request(method, url, params=params, data=data) as resp:
-                return await resp
-                #request(method, url, *, params=None, data=None, json=None, headers=None, skip_auto_headers=None, auth=None, allow_redirects=True, max_redirects=10, compress=None, chunked=None, expect100=False, read_until_eof=True, proxy=None, proxy_auth=None, timeout=5*60, verify_ssl=None, fingerprint=None, ssl_context=None, proxy_headers=None
+    def get(self, url, params=None, data=None, headers=defaultHeaders, cookies=None, files=None, auth=None, timeout=None, allowRedirects=False, proxies=None, hooks=None, stream=False, verify=None, cert=None):
+        response = await requests.get(url.replace(' ', '%20'), params=params, data=data, headers=headers, cookies=cookies, files=files, auth=auth, timeout=timeout, allowRedirects=allowRedirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert)
+        return response
+    def request(self, method, url, params=None, data=None, headers=defaultHeaders, cookies=None, files=None, auth=None, timeout=None, allowRedirects=False, proxies=None, hooks=None, stream=False, verify=None, cert=None):
+        return requests.request(method=method, url=url, params=params, data=data, headers=headers, cookies=cookies, files=files, auth=auth, timeout=timeout, allow_redirects=allowRedirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert)
+    def post(self, url, params=None, data=None, headers=defaultHeaders, cookies=None, files=None, auth=None, timeout=None, allowRedirects=False, proxies=None, hooks=None, stream=False, verify=None, cert=None):
+        return requests.post(url=url.replace(' ', '%20'), params=params, data=data, headers=headers, cookies=cookies, files=files, auth=auth, timeout=timeout, allow_redirects=allowRedirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert)
+    def put(self, url, params=None, data=None, headers=defaultHeaders, cookies=None, files=None, auth=None, timeout=None, allowRedirects=False, proxies=None, hooks=None, stream=False, verify=None, cert=None):
+        return requests.put(url=url.replace(' ', '%20'), params=params, data=data, headers=headers, cookies=cookies, files=files, auth=auth, timeout=timeout, allow_redirects=allowRedirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert)
+    def delete(self, url, params=None, data=None, headers=defaultHeaders, cookies=None, files=None, auth=None, timeout=None, allowRedirects=False, proxies=None, hooks=None, stream=False, verify=None, cert=None):
+        return requests.delete(url=url.replace(' ', '%20'), params=params, data=data, headers=headers, cookies=cookies, files=files, auth=auth, timeout=timeout, allow_redirects=allowRedirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert)
+    def head(self, url, params=None, data=None, headers=defaultHeaders, cookies=None, files=None, auth=None, timeout=None, allowRedirects=False, proxies=None, hooks=None, stream=False, verify=None, cert=None):
+        return requests.head(url=url.replace(' ', '%20'), params=params, data=data, headers=headers, cookies=cookies, files=files, auth=auth, timeout=timeout, allow_redirects=allowRedirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert)
+    def options(self, url, params=None, data=None, headers=defaultHeaders, cookies=None, files=None, auth=None, timeout=None, allowRedirects=False, proxies=None, hooks=None, stream=False, verify=None, cert=None):
+        return requests.options(url=url.replace(' ', '%20'), params=params, data=data, headers=headers, cookies=cookies, files=files, auth=auth, timeout=timeout, allow_redirects=allowRedirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert)
