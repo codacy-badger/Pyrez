b3d26a19a6134c2151989674c7488f219a6b26d3
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-03-23 14:32:03 -0300

    Update http.py

--- pyrez/http.py
+++ pyrez/http.py
@@ -10,7 +10,7 @@ class HttpRequest():
         self.headers=defaultHeaders if headers is None else headers
 
     async def get(self, url, params=None, data=None, headers=defaultHeaders, cookies=None, files=None, auth=None, timeout=None, allowRedirects=False, proxies=None, hooks=None, stream=False, verify=None, cert=None):
-        return await requests.get(url.replace(' ', '%20'), params=params, data=data, headers=headers, cookies=cookies, files=files, auth=auth, timeout=timeout, allowRedirects=allowRedirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert)
+        return await requests.get(url.replace(' ', '%20'), params=params, data=data, headers=headers)#, cookies=cookies, files=files, auth=auth, timeout=timeout, allowRedirects=allowRedirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert)
     async def request(self, method, url, params=None, data=None, headers=defaultHeaders, cookies=None, files=None, auth=None, timeout=None, allowRedirects=False, proxies=None, hooks=None, stream=False, verify=None, cert=None):
         return await requests.request(method=method, url=url, params=params, data=data, headers=headers, cookies=cookies, files=files, auth=auth, timeout=timeout, allow_redirects=allowRedirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert)
     async def post(self, url, params=None, data=None, headers=defaultHeaders, cookies=None, files=None, auth=None, timeout=None, allowRedirects=False, proxies=None, hooks=None, stream=False, verify=None, cert=None):
